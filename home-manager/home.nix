26e11c6e7ceb2cdf3ba04d0b3ce489407716d43f
tomiock <ockier1@gmail.com>
2024-07-29 11:05:44 +0200

    gtk stuff

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -107,11 +107,26 @@
     };
   };
 
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
   gtk = {
     enable = true;
-    theme.name = "adw-gtk3";
-    cursorTheme.name = "Bibata-Modern-Ice";
-    iconTheme.name = "GruvboxPlus";
+
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+    };
   };
 
   services.mako = with config.colorScheme.palette; {
