d5e1b8cc9dcb87aa38a91f39b8b1d8fdec215e63
tomiock <ockier1@gmail.com>
2024-08-08 13:43:24 +0200

    firefox search engines

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -10,6 +10,28 @@
         vimium
         ublock-origin
       ];
+
+      search.engines = {
+        "Nix Packages" = {
+          urls = [{
+            template = "https://search.nixos.org/packages";
+            params = [
+              { name = "type"; value = "packages"; }
+              { name = "query"; value = "{searchTerms}"; }
+            ];
+          }];
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@np" ];
+        };
+
+        "Home Manager Options" = {
+          urls = [{
+            template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=release-24.05";
+          }];
+
+          definedAliases = [ "@mo" ];
+        };
+      };
     };
   };
 }
