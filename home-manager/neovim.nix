28add114306683a2656082a4c119363f8ad2b323
tomiock <ockier1@gmail.com>
2024-08-21 14:32:04 +0200

    surf dependencies

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,16 +1,21 @@
 { config, pkgs, inputs, lib, ... }:
 {
   home.packages = [
-    pkgs.nil # TODO: install with flakes
-    pkgs.python311Packages.python-lsp-server
-    pkgs.luajitPackages.lua-lsp
     pkgs.luajitPackages.luarocks
     pkgs.tree-sitter
     pkgs.nodejs_22
+
+    # LSPs
     pkgs.rocmPackages_5.llvm.clang-tools-extra
+    pkgs.nil # TODO: install with flakes
     pkgs.lua-language-server
+    pkgs.python311Packages.python-lsp-server
+    pkgs.luajitPackages.lua-lsp
     pkgs.rust-analyzer
     pkgs.gopls
+
+    pkgs.libcaca # SURF
+    pkgs.mplayer #
   ];
 
   programs.neovim = {
